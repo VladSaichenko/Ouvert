05b21a049865df0bb31ed22c9ffdd180a8b64015
VladSaichenko <reallinuxforlinux@gmail.com>
2020-08-31 15:13:58 +0700

    Added test, debuged users

--- server/apps/users/viewsets/users.py
+++ server/apps/users/viewsets/users.py
@@ -6,16 +6,12 @@ from rest_framework.status import HTTP_201_CREATED
 
 from django.contrib.auth.models import User
 
-from url_filter.integrations.drf import DjangoFilterBackend
-
 from apps.users.serializers.serializers import UserSerializer
 
 
 class UserViewSet(CreateModelMixin, GenericViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    filter_backends = (DjangoFilterBackend,)
-    filter_fields = ('username', 'first_name', 'last_name',)
 
     def create(self, request, *args, **kwargs):
         serializer = self.serializer_class(
@@ -31,5 +27,10 @@ class UserViewSet(CreateModelMixin, GenericViewSet):
         user.set_password(serializer.validated_data['password'])
         user.save()
         token = Token.objects.create(user=user).key
+        response_data = {
+            'id': user.id,
+            'username': user.username,
+            'token': token
+        }
 
-        return Response({'username': user.username, 'token': token}, status=HTTP_201_CREATED)
+        return Response(response_data, status=HTTP_201_CREATED)
